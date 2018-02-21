71cc33390d2a41f4402bcccd9aaae5d79db71a5b
ValysyaPoncho <valentin.ivancikov@gmail.com>
2018-02-21 22:29:49 +0300

    second solution 2.0

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = [2, 3, 1, 5, 6, 4]) {
   var sum = 0, len = preferences.length;
-  
+
   for (var i = 0; i < preferences.length; i++) {
     if (preferences[i] !== 0) {
       var num1 = preferences[i], num2 = preferences[num1 - 1], num3 = preferences[num2 - 1];
@@ -18,22 +18,3 @@ module.exports = function getLoveTrianglesCount(preferences = [2, 3, 1, 5, 6, 4]
 
   return sum;
 };
-
-/*function getLoveTrianglesCount(preferences = [2, 3, 1, 5, 6, 4]) {
-  var sum = 0, len = preferences.length;
-  for (var i = 0; i < preferences.length; i++) {
-    if (preferences[i] !== 0) {
-      var num1 = preferences[i], num2 = preferences[num1 - 1], num3 = preferences[num2 - 1];
-
-        if ((i + 1 === num3) && (preferences[i] !== 0) && (num1 !== num2) && (num2 !== num3) && (num3 !== num1)) {
-          ++sum;
-          preferences[i] = preferences[num1 - 1] = preferences[num2 - 1] = 0; 
-        }
-    }
-  }
-
-  return sum;
-}
-
-let count = getLoveTrianglesCount([2, 3, 1, 5, 6, 4]);
-console.log(count);*/
